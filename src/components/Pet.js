53e7f79f362f2242b0d94f24ea67aee6c8011bc2
chrislemus <icrislemus@gmail.com>
2021-04-14 11:44:06 -0400

    commit tree 54949ed8ab47b8899cd4475a101314b8b2f39a18

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,29 +1,36 @@
-import React from 'react'
+import React from "react";
 
 class Pet extends React.Component {
   render() {
+    const { age, gender, id, isAdopted, type, weight, name } = this.props.pet;
     return (
       <div className="card">
         <div className="content">
-          <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
-          </a>
+          <h1>{name}</h1>
+          <a className="header">{gender === "male" ? "♂" : "♀"}</a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {age}</p>
+            <p>Weight: {weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          {isAdopted ? (
+            <button className="ui disabled button">Already adopted</button>
+          ) : (
+            <button
+              className="ui primary button"
+              onClick={() => this.props.onAdoptPet(id)}
+            >
+              Adopt pet
+            </button>
+          )}
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Pet
+export default Pet;
